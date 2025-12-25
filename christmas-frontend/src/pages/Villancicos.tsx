d41e578530486c385c96c0c9a70fc2eda71a844f
Gian Anthony Choque Quispe <giananthonych@gmail.com>
2025-12-24 19:51:55 -0500

    fix: remove 100mb frontend limit check

--- christmas-frontend/src/pages/Villancicos.tsx
+++ christmas-frontend/src/pages/Villancicos.tsx
@@ -44,9 +44,7 @@ export default function Villancicos() {
         const file = e.target.files?.[0];
         if (!file) return;
 
-        if (file.size > 100 * 1024 * 1024) {
-            return toast.error('El video debe pesar menos de 100MB');
-        }
+
 
         setIsUploading(true);
         const toastId = toast.loading('Subiendo villancico...');
